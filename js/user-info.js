e28152088a4a19ad86cce7c2c2d41543eb698ba0
ailin-p <ailinparedes97@gmail.com>
2023-10-24 15:38:00 -0300

    pagina users

--- js/user-info.js
+++ js/user-info.js
@@ -5,37 +5,62 @@ const headerUserInfoLogin = document.getElementById("loginHeader")
 const headerUserInfoRegister = document.getElementById("registerHeader")
 const headerForAdmin = document.getElementById("navbarSupportedContent")
 
-//Si hay un usuario logueado, ponemos nombre y pintamos botón de cerrar sesión
+// Si hay un usuario logueado, ponemos nombre y pintamos botón de cerrar sesión
 if (currentUser) {
     headerUserInfoLogin.innerText = currentUser.fullname;
     headerUserInfoRegister.innerHTML = `<button onclick="logout()" style="color: #FF7A00; border:0">cerrar sesión</button>`;
 
-    //Si ademas ese usuario es admin, mostrar página admin
+    // Si además ese usuario es admin, mostrar página admin
     if (currentUser.role === "ROLE_ADMIN") {
-        const newNavItem = document.createElement('li');
-        newNavItem.classList.add('nav-item');
-    
-        const newNavLink = document.createElement('a');
-        newNavLink.classList.add('nav-link', 'text-reset');
-        newNavLink.href = '/pages/admin/admin.html';
-        newNavLink.textContent = 'admin';
-        // Añade a <a> como hijo de <li>
-        newNavItem.appendChild(newNavLink);
-        // Captura el nav-bar para que el elemento que se creó anteriormente sea hijo del ul con clase .navbar-nav
-        const navList = document.querySelector('.navbar-nav');
-        navList.appendChild(newNavItem);
+        const productsNavItem = document.querySelector('a[href="/pages/admin/admin.html"]');
+        if (!productsNavItem) {
+            const newProductsNavItem = createNavItem('productos', '/pages/admin/admin.html');
+            insertNavItem(newProductsNavItem);
+        }
+        
+        const usersNavItem = document.querySelector('a[href="/pages/users/users.html"]');
+        if (!usersNavItem) {
+            const newUsersNavItem = createNavItem('usuarios', '/pages/users/users.html');
+            insertNavItem(newUsersNavItem);
+        }
     }
 } else {
     headerUserInfoLogin.innerHTML = `<li id="loginHeader"><a class="text-reset text-decoration-none" href="/pages/login/login.html">iniciar sesión</a></li>`;
     headerUserInfoRegister.innerHTML = `<li id="registerHeader"><a class="text-reset text-decoration-none" href="/pages/register/register.html">registrarse</a></li>`;
     
-    // Verificamos si existe el elemento "admin" antes de intentar eliminarlo
-    const adminNavItem = document.querySelector('a[href="/pages/admin/admin.html"]');
-    if (adminNavItem) {
+    // Verificamos si existen los elementos "productos" y "usuarios" antes de intentar eliminarlos
+    const productsNavItem = document.querySelector('a[href="/pages/admin/admin.html"]');
+    const usersNavItem = document.querySelector('a[href="/pages/users/users.html"]');
+    if (productsNavItem) {
         const navList = document.querySelector('.navbar-nav');
-        navList.removeChild(adminNavItem.parentElement);
+        navList.removeChild(productsNavItem.parentElement);
+    }
+    if (usersNavItem) {
+        const navList = document.querySelector('.navbar-nav');
+        navList.removeChild(usersNavItem.parentElement);
     }
 }
+
+// Función para crear un nuevo elemento de navegación
+function createNavItem(text, href) {
+    const newNavItem = document.createElement('li');
+    newNavItem.classList.add('nav-item');
+    
+    const newNavLink = document.createElement('a');
+    newNavLink.classList.add('nav-link', 'text-reset');
+    newNavLink.href = href;
+    newNavLink.textContent = text;
+    
+    newNavItem.appendChild(newNavLink);
+    return newNavItem;
+}
+
+// Función para insertar un elemento de navegación en la barra de navegación
+function insertNavItem(navItem) {
+    const navList = document.querySelector('.navbar-nav');
+    navList.appendChild(navItem);
+}
+
 
 
 //Función para desloguear
